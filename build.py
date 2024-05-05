9c9d57246de6c3e6716155ce42e331490f448404
pancake423 <58053103+pancake423@users.noreply.github.com>
2024-05-05 17:57:27 -0400

    rebuilt exe

--- build.py
+++ build.py
@@ -1,7 +1,18 @@
 import PyInstaller.__main__
+import shutil
+import os
 
 PyInstaller.__main__.run([
     'WizardArcade.py',
+    '-y',
     '--noconsole',
     '--icon=favicon.ico'
 ])
+
+try:
+    shutil.rmtree("_internal")
+    os.remove("WizardArcade.exe")
+except FileNotFoundError:
+    pass
+shutil.move("dist/WizardArcade/_internal", os.getcwd())
+shutil.move("dist/WizardArcade/WizardArcade.exe", os.getcwd())
